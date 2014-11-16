050392d5bc43532b182ef3334719701e0d676b4a
Julia.Reutskaya <reutskajulia@ukr.net>
2014-11-16 22:15:26 +0200

    add ajax loader

--- app/main.js
+++ app/main.js
@@ -2,5 +2,67 @@
  * Created by ulia on 16.11.14.
  */
 $(function(){
-    alert('work');
+
+    var tasks = null,
+        tabTaskContainer = $("#tabs"),
+        contentContainer = $("#content");
+
+
+    loadProjectStructure ();
+
+    function loadProjectStructure () {
+
+        var url = "./app/structure.json";
+
+        $.ajax({
+            url: url,
+            contentType: "json"
+        }).done(function (response) {
+            tasks = response.tasks;
+            loadTask ();
+        });
+    }
+
+
+    function loadTask () {
+        $.each(tasks, function () {
+
+            var taskNameTab = $("<li>"),
+                taskNameA = $("<a>");
+
+            taskNameA.text(this);
+            taskNameA.attr("href", "#");
+            taskNameTab.append(taskNameA);
+            tabTaskContainer.append(taskNameTab);
+
+            loadTaskContent(this);
+        });
+
+    }
+
+    function loadTaskContent(name) {
+
+        var url = "./app/tasks/" + name + "/index.html";
+
+        $.ajax({
+            url: url,
+            contentType: "text/html"
+        }).done(function (response) {
+            var startBody = response.indexOf("<body>") + 6,
+                endBody = response.indexOf("</body>"),
+                loadedBody = response.substring(startBody, endBody),
+                oneContent = $("<li>");
+
+            oneContent.html(loadedBody);
+            contentContainer.append(oneContent)
+            initStyles();
+        });
+    }
+
+    function initStyles() {
+        if(contentContainer.children().length === 1) {
+            contentContainer.children().first().addClass('active');
+            tabTaskContainer.children().first().addClass('active');
+        }
+    }
 });
---
+++ app/tasks/summator/scripts.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ulia on 16.11.14.
+ */
+
