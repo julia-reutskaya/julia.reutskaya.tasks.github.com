6aec86290199f10df6eb39f3cf654d7a4b1f8a87
Julia.Reutskaya <reutskajulia@ukr.net>
2014-11-16 22:34:18 +0200

    summator

--- app/tasks/summator/scripts.js
+++ app/tasks/summator/scripts.js
@@ -1,4 +1,25 @@
 /**
  * Created by ulia on 16.11.14.
  */
+var arr = [66, 87, null, 105, "mama", 30, 25, "mula", 0, 125];
 
+console.log(sumArr(arr));
+
+function sumArr (arr) {
+
+    var result = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (validate(arr[i])){
+            result += arr[i];
+
+        }
+    }
+
+    return result;
+
+}
+
+function validate(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n) && n > 100;
+}
